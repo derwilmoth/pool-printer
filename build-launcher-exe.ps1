ac8122e182058fb7f4c33387dd332bee56077328
selim <selimyag.eser@gmail.com>
2026-04-23 04:24:59 +0200

    feat: added icon to launcher exe

--- build-launcher-exe.ps1
+++ build-launcher-exe.ps1
@@ -5,8 +5,16 @@ param(
 
 $ErrorActionPreference = "Stop"
 
+$ScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+$IconPath = Join-Path $ScriptRoot "public\icon.ico"
+
 if (-not (Test-Path $InputScript)) {
   throw "Input script not found: $InputScript"
+}
+
+if (-not (Test-Path $IconPath)) {
+  throw "Icon not found: $IconPath"
 }
 
 if (-not (Get-Module -ListAvailable -Name ps2exe)) {
@@ -16,6 +24,12 @@ if (-not (Get-Module -ListAvailable -Name ps2exe)) {
 
 Import-Module ps2exe -ErrorAction Stop
 
-Invoke-ps2exe -InputFile $InputScript -OutputFile $OutputExe -NoConsole -Title "Pool Printer Launcher" -Product "Pool Printer"
+Invoke-ps2exe `
+  -InputFile $InputScript `
+  -OutputFile $OutputExe `
+  -NoConsole `
+  -Title "Pool Printer Launcher" `
+  -Product "Pool Printer" `
+  -IconFile $IconPath
 
 Write-Host "Launcher EXE created: $OutputExe"
